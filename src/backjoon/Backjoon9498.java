41815c8025a8c200ba2d42e3ee9059ae27c57d3f
jiaekim123 <hirlawldo413@gmail.com>
2019-08-16 17:27:46 +0900

    [Backjoon] for문 문제 해결 (2739, 9498)

--- src/backjoon/Backjoon9498.java
+++ src/backjoon/Backjoon9498.java
@@ -11,13 +11,13 @@ public class Backjoon9498 {
     }
 
     public static char grades(int score) {
-        if (score>=90 && score <=100 ){
+        if (score >= 90 && score <= 100) {
             return 'A';
-        } else if (score>=80 && score <=89 ){
+        } else if (score >= 80 && score <= 89) {
             return 'B';
-        } else if (score>=70 && score <=79 ){
+        } else if (score >= 70 && score <= 79) {
             return 'C';
-        } else if (score>=60 && score <=69 ){
+        } else if (score >= 60 && score <= 69) {
             return 'D';
         } else {
             return 'F';
