c8fef8914f33d8eea93696c59bd720e9d94bb029
jiae <37948906+jiaekim123@users.noreply.github.com>
2020-10-20 10:47:33 +0900

    [programmers] 완주하지 못한 선수 List -> HashMap으로 변경해서 효율성 테스트 통과

--- src/programmers/courses/lesson68981/NotFinishedPlayer.java
+++ src/programmers/courses/lesson68981/NotFinishedPlayer.java
@@ -1,23 +1,34 @@
 package courses.lesson68981;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
 
 class Solution {
-    public String solution(String[] participant, String[] completion) {
-        List<String> participantList = new ArrayList<String>(Arrays.asList(participant));
-        
-        for (String player : completion) {
-            participantList.remove(participantList.indexOf(player));
-        }
-        
-        return participantList.get(0);
-    }
+	public String solution(String[] participant, String[] completion) {
+		HashMap<String, Integer> participantMap = new HashMap<String, Integer>();
+		for (String player : participant) {
+			Integer playerCount = participantMap.getOrDefault(player, 0);
+			if (playerCount != 0) {
+				participantMap.replace(player, playerCount + 1);
+			} else {
+				participantMap.put(player, 1);
+			}
+		}
+		
+		for (String player : completion) {
+			Integer playerCount = participantMap.getOrDefault(player, 0);
+			if (playerCount > 1) {
+				participantMap.replace(player, playerCount - 1);
+			} else {
+				participantMap.remove(player);
+			}
+		}
+		return participantMap.keySet().iterator().next();
+	}
 }
 
 public class NotFinishedPlayer {
 	public static void main(String[] args) {
-		System.out.println(new Solution().solution(new String[] {"leo", "kiki", "eden"}, new String[] {"eden", "kiki"}));
+		System.out.println(
+				new Solution().solution(new String[] { "leo", "kiki", "eden" }, new String[] { "eden", "kiki" }));
 	}
 }
