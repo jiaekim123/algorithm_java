382bd54193e3bd290701e768298a9adc2a7c6b69
jiae <hirlawldo413@gmail.com>
2020-10-21 17:26:57 +0900

    [programmers] 주식가격 68982 문제 해결 (효율성 문제 해결)

--- src/programmers/courses/lesson68982/StockPrice.java
+++ src/programmers/courses/lesson68982/StockPrice.java
@@ -1,25 +1,16 @@
 package courses.lesson68982;
 
-import java.util.Arrays;
-
 class Solution {
 	public int[] solution(int[] prices) {
 		int[] answer = new int[prices.length];
-		boolean[] isIncrease = new boolean[prices.length];
-		Arrays.fill(isIncrease, true);
-
 		for (int i = 0; i < prices.length; i++) {
-			answer[i] = 0;
-			for (int j = i - 1; j >= 0; j--) {
-				if (isIncrease[j]) {
-					answer[j]++;
-					if (prices[i] < prices[j]) {
-						isIncrease[j] = false;
-					}
+			for (int j = i + 1; j < prices.length; j++) {
+				answer[i]++;
+				if (prices[i] > prices[j]) {
+					break;
 				}
 			}
 		}
-
 		return answer;
 	}
 }
