e747ce2f5f106a122da8ed6260041f7ef885667f
jiae <37948906+jiaekim123@users.noreply.github.com>
2020-10-26 16:18:51 +0900

    [programmers] 다리를 지나는 트럭 문제 해결 (68984번)

--- src/programmers/courses/lesson68984/TruckBridge.java
+++ src/programmers/courses/lesson68984/TruckBridge.java
@@ -1,29 +1,50 @@
 package courses.lesson68984;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
 class Solution {
-    public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int time = 0;
-        Bridge bridge = new Bridge(bridge_length, weight);
-        for (int i = 0; i<truck_weights.length; i++) {
-        	
-        	
-        }
-        return time;
-    }
+	public int solution(int bridge_length, int weight, int[] truck_weights) {
+		int time = 0;
+		Bridge bridge = new Bridge(bridge_length, weight);
+		Queue<Truck> truckQueue = new LinkedList<Truck>();
+		for (int truck_weight : truck_weights) {
+			truckQueue.offer(new Truck(truck_weight));
+		}
+
+		while (!truckQueue.isEmpty() || !bridge.isBridgeEmpth()) {
+			time++;
+			bridge.removeTruck();
+			bridge.addTruck(truckQueue);
+			bridge.moveTruck();
+		}
+		return time;
+	}
 }
 
 class Truck {
-	int weight;
-	int position;
-	
-	public Truck(int weight, int position) {
+	private int weight;
+	private int position;
+
+	public Truck(int weight) {
 		this.weight = weight;
-		this.position = position;
+		this.position = 0;
+	}
+
+	public void moveTruck(int position) {
+		this.position += position;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public void moveTruck() {
+		++this.position;
 	}
 }
 
@@ -31,39 +52,47 @@ class Bridge {
 	private int bridgeWeight;
 	private int truckWeights;
 	private int bridgeLength;
-	private int time;
-	private List<Truck> truckList = new ArrayList<Truck>();
-	
-	public Bridge(int bridgeLength, int birdgeWeight) {
+	private Queue<Truck> bridgeQueue;
+
+	public Bridge(int bridgeLength, int bridgeWeight) {
 		this.bridgeLength = bridgeLength;
 		this.bridgeWeight = bridgeWeight;
 		this.truckWeights = 0;
-		this.time= 0;
+		this.bridgeQueue = new LinkedList<Truck>();
 	}
-	
-	public boolean addTruck(Truck truck) {
-		if (truckWeights + truck.weight < bridgeWeight) {
-			truckList.add(truck);
-			truckWeights += truck.weight;
+
+	public boolean addTruck(Queue<Truck> truckQueue) {
+		if (!truckQueue.isEmpty() && truckQueue.peek().getWeight() + truckWeights <= bridgeWeight) {
+			bridgeQueue.add(truckQueue.peek());
+			truckWeights += truckQueue.poll().getWeight();
 			return true;
 		}
 		return false;
 	}
-	
-	public void addTime(int time) {
-		this.time += time;
+
+	public void removeTruck() {
+		if (!bridgeQueue.isEmpty()) {
+			if (bridgeQueue.peek().getPosition() == bridgeLength) {
+				truckWeights -= bridgeQueue.poll().getWeight();
+			}
+		}
 	}
-	
-	public int getTime() {
-		return time;
+
+	public void moveTruck() {
+		for (Truck truck : bridgeQueue) {
+			truck.moveTruck();
+		}
 	}
-	
+
+	public boolean isBridgeEmpth() {
+		return this.bridgeQueue.isEmpty();
+	}
 }
 
 public class TruckBridge {
 	public static void main(String[] args) {
-		System.out.println(new Solution().solution(2, 10, new int[] {7,4,5,6}));
+//		System.out.println(new Solution().solution(2, 10, new int[] { 7, 4, 5, 6 }));
 //		System.out.println(new Solution().solution(100, 100, new int[] {10}));
-//		System.out.println(new Solution().solution(100, 100, new int[] {10,10,10,10,10,10,10,10,10,10}));
+		System.out.println(new Solution().solution(100, 100, new int[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 }));
 	}
 }
