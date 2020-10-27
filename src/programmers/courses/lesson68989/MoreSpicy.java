3a5edfd86523cc09f5df6fe62c4f3c25f5e9d73b
jiae <hirlawldo413@gmail.com>
2020-10-27 13:34:01 +0900

    [programmers] 더 맵게 (68989) 타임아웃

--- src/programmers/courses/lesson68989/MoreSpicy.java
+++ src/programmers/courses/lesson68989/MoreSpicy.java
@@ -5,10 +5,13 @@ import java.util.Arrays;
 class Solution {
 	public int solution(int[] scoville, int K) {
 		int answer = 0;
+		
 		while (true) {
-			if ((scoville = mixScovile(scoville, K)) == null) {
-				break;
-			}
+			Arrays.sort(scoville);
+			int checkValue = checkScovile(scoville, K);
+			if (checkValue == 1) break;
+			else if (checkValue == -1) return -1;
+			scoville = mixScovile(scoville, K);
 			answer++;
 		}
 		return answer;
@@ -16,21 +19,28 @@ class Solution {
 
 	private int[] mixScovile(int[] scoville, int K) {
 		int[] newScoville = new int[scoville.length - 1];
-		Arrays.sort(scoville);
-		if (scoville[0] >= K) {
-			return null;		
-		}
-		
 		newScoville[0] = scoville[0] + (scoville[1] * 2);
 		for (int i = 1; i < newScoville.length; i++) {
 			newScoville[i] = scoville[i + 1];
 		}
 		return newScoville;
 	}
+	
+	// return // -1: 모든 원소를 K이상으로 할 수 없음 / 0: 다시 한번 더 믹스 / 1: 완료
+	private int checkScovile(int[] scoville, int K) {
+		if (scoville.length <= 1) return -1;
+		if (scoville[0] >= K) {
+			return 1;		
+		}
+		return 0;
+		
+	}
 }
 
 public class MoreSpicy {
 	public static void main(String[] args) {
 		System.out.println(new Solution().solution(new int[] { 1, 2, 3, 9, 10, 12 }, 7));
+		System.out.println(new Solution().solution(new int[] { 1, 1, 1, 1, 1, 1}, 7));
+
 	}
 }
