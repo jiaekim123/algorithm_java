469ad1225cf349e2791c5d0801684e0180f8c915
jiae <hirlawldo413@gmail.com>
2020-11-10 12:53:34 +0900

    [programmers] 도둑질 문제 수정
    메서드 두개로 있던 것 하나로 합침

--- src/programmers/courses/lesson69001/Stilling.java
+++ src/programmers/courses/lesson69001/Stilling.java
@@ -1,28 +1,18 @@
 package courses.lesson69001;
 
 class Solution {
-    public int solution(int[] money) {
-    	return Math.max(includeFirstor(money), excludeFirst(money));
-    }
+	public int solution(int[] money) {
+		return Math.max(dp(money, money[0], money[0], money.length - 1), dp(money, 0, money[1], money.length));
+	}
     
-    public int includeFirstor(int[] money) {
+    public int dp(int[] money, int first, int second, int last) {
     	int max[] = new int[money.length];
-    	max[0] = money[0];
-    	max[1] = money[0];
-    	for (int i = 2; i < money.length-1; i++) {
+    	max[0] = first;
+    	max[1] = second;
+    	for (int i = 2; i < last; i++) {
     		max[i] = Math.max(max[i-1], max[i-2] + money[i]);
     	}
-    	return max[money.length-2];
-    }
-    
-    public int excludeFirst(int[] money) {
-    	int max[] = new int[money.length];
-    	max[0] = 0;
-    	max[1] = money[1];
-    	for (int i = 2; i < money.length; i++) {
-    		max[i] = Math.max(max[i-1], max[i-2] + money[i]);
-    	}
-    	return max[money.length-1];
+    	return max[last-1];    	
     }
 }
 
